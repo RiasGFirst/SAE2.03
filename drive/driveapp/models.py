96a3c59b5c237a6b46c04b85542996d1624b0895
RiasGFirst <gremoryrias69@proton.me>
2024-05-16 11:35:39 +0200

    V0.0.1 C.R.U.D Not Working have to finish

---
+++ drive/driveapp/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+
+# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    forname = models.CharField(max_length=100)
+    inscription_date = models.DateField(auto_now=True)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}, {self.forname}, {self.inscription_date}, {self.address}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    date_peremption = models.DateField()
+    image = models.ImageField(upload_to='static/driveapp/images/products/')
+    brand = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name}, {self.price}, {self.date_peremption}, {self.brand}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}, {self.description}"
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    date = models.DateField(auto_now=True)
+    status = models.CharField(max_length=100)
+    products = models.ManyToManyField(Product, through='List')
+
+    def __str__(self):
+        return f"{self.client}, {self.number}, {self.date}, {self.status}"
+
+
+class List(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.order}, {self.product}, {self.quantity}"
+
---
+++ drive/driveapp/views/views_order.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, HttpResponseRedirect
+from ..forms import OrderForm
+from .. import models
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'driveapp/order/home.html')
+def add(request):
+    form = OrderForm()
+    return render(request, 'driveapp/order/add.html', {'form': form})
+
+
+def processing(request):
+    form = OrderForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return render(request, 'driveapp/lorder/success.html', {'order': form.cleaned_data})
+    else:
+        return render(request, 'driveapp/order/error.html')
+
+
+def update(request, id):
+    order = models.Order.objects.get(id=id)
+    form = OrderForm(instance=order)
+    return render(request, 'driveapp/order/update.html', {'form': form, 'order': order})
+
+
+def processing_update(request, id):
+    lform = OrderForm(request.POST)
+    if lform.is_valid():
+        order = lform.save(commit=False)
+        order.id = id
+        order.save()
+        return HttpResponseRedirect("/order/")
+    else:
+        return render(request, "driveapp/order/update.html", {"form": lform, "id": id})
+
+
+def show(request, id):
+    order = models.Order.objects.get(id=id)
+    return render(request, 'driveapp/order/show.html', {'order': order})
+
+
+def delete(request, id):
+    order = models.Order.objects.get(id=id)
+    order.delete()
+    return HttpResponseRedirect("/order/")
